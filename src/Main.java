ec11545b36e381131349ee80b84b6172d62cc16b
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-11-16 12:57:00 +0000

    Productor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Productor primeraPlanta = new Productor(2, 5, 26, 8);
+
+        primeraPlanta.start();
     }
 }
--- src/Productor.java
+++ src/Productor.java
@@ -1,2 +1,39 @@
-public class Productor {
+import java.util.ArrayList;
+
+public class Productor extends Thread{
+
+    private int clientes;
+    private int productor;
+    private int limitePlantacion;
+    private int plantasCreciendo;
+
+    public String[] Vegetales = {"lettuce", "cabbage", "onion", "spinach", "potato", "celery",
+            "asparagus", "radish", "broccoli", "artichoke", "tomato",
+            "cucumber", "eggplant", "carrot", "green bean"};
+
+    private ArrayList<String> plantasCrecidas = new ArrayList<>();
+
+    public Productor(int clientes, int productor, int limitePlantacion, int plantasCreciendo) {
+        this.clientes = clientes;
+        this.productor = productor;
+        this.limitePlantacion = limitePlantacion;
+        this.plantasCreciendo = plantasCreciendo;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            for (int i = 0; i < this.plantasCreciendo; i++){
+                int randomAccess = ((int) (Math.random() * 15));
+                int TIEMPOCRECIMIENTO = (int)( Math.random() * 900);
+                sleep((long) TIEMPOCRECIMIENTO);
+                System.out.println("La planta: " + this.Vegetales[randomAccess] + " ha crecido con un tiempo de: " + TIEMPOCRECIMIENTO + " minutos");
+                plantasCrecidas.add(this.Vegetales[randomAccess]);
+            }
+         }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
+
